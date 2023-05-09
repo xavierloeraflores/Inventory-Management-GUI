06ca46370bc9f3267a534aa2887f9c2754088b1a
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-09 09:14:51 -0700

    DOCS: Added @author to main class

--- src/main/java/com/xavierloeraflores/inventorymanagement/Main.java
+++ src/main/java/com/xavierloeraflores/inventorymanagement/Main.java
@@ -1,5 +1,8 @@
 package com.xavierloeraflores.inventorymanagement;
-
+/**
+ *
+ * @author xavierloeraflores
+ */
 public class Main {
     public static void main(String[] args){
         System.out.println("Inventory Management Application");
