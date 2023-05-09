900d31f912f65c7a4a0382c5f1d0a7a6c400b56f
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-09 09:11:48 -0700

    FEAT: Created Main class

---
+++ src/main/java/com/xavierloeraflores/inventorymanagement/Main.java
@@ -0,0 +1,8 @@
+package com.xavierloeraflores.inventorymanagement;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Inventory Management Application");
+    }
+}
+
