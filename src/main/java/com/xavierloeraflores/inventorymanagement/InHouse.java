19a9e47a46330bb663b70ab9c1771ac1b74374b9
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-11 13:54:13 -0700

    FEAT: Added machineId property to InHouse class

--- src/main/java/com/xavierloeraflores/inventorymanagement/InHouse.java
+++ src/main/java/com/xavierloeraflores/inventorymanagement/InHouse.java
@@ -1,7 +1,9 @@
 package com.xavierloeraflores.inventorymanagement;
 
 public class InHouse extends Part{
-    public InHouse(int id, String name, double price, int stock, int min, int max) {
+    private int machineId;
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
+        this.machineId = machineId;
     }
 }
