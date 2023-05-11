b48e2dee2b68efe8a3e23df7ee1060974b6f0373
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-11 13:52:37 -0700

    FEAT: Created InHouse class

---
+++ src/main/java/com/xavierloeraflores/inventorymanagement/InHouse.java
@@ -0,0 +1,7 @@
+package com.xavierloeraflores.inventorymanagement;
+
+public class InHouse extends Part{
+    public InHouse(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+    }
+}
