9d3cd039e21dca6d13619e2e4e71435e7412acc8
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-13 12:43:34 -0700

    DOCS: Generated docs

---
+++ docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.xavierloeraflores.inventorymanagement"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.xavierloeraflores.inventorymanagement","l":"com.xavierloeraflores.inventorymanagement"}];updateSearchResults();
