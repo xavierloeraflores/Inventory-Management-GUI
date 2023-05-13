a7b2504e14a11439e6936431c2fc42c6f83f4438
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-13 12:21:45 -0700

    DOCS: lookupProduct() & lookupPart() functions

--- src/main/java/com/xavierloeraflores/inventorymanagement/Inventory.java
+++ src/main/java/com/xavierloeraflores/inventorymanagement/Inventory.java
@@ -29,6 +29,11 @@ public class Inventory {
     public void newProduct(Product newProduct){
         this.allProducts.add(newProduct);
     }
+    /**
+     *  Looks up a part given a partID and returns the result.
+     * @param partId [int] partId value to be looked up
+     * @return Part matching the given partId value or null if not found
+     */
     public Part lookupPart(int partId){
         for(int i = 0; i<this.allParts.size(); i++){
             if(this.allParts.get(i).getId() == partId){
@@ -37,6 +42,11 @@ public class Inventory {
         }
         return null;
     }
+    /**
+     *  Looks up a product given a productId and returns the result.
+     * @param productId [int] productId value to be looked up
+     * @return Product matching the given productId value or null if not found
+     */
     public Product lookupProduct(int productId){
         for(int i = 0 ; i<this.allProducts.size(); i++){
             if(this.allProducts.get(i).getId()==productId){
@@ -45,6 +55,11 @@ public class Inventory {
         }
         return null;
     }
+    /**
+     *  Looks up parts given a partName and returns the result
+     * @param partName [string] value partName to be looked up
+     * @return ObservableList<Part> matching the given partName value or null if not found
+     */
     public ObservableList<Part> lookupPart(String partName){
         ObservableList<Part> parts = FXCollections.observableArrayList();
         for(int i =  0; i<this.allParts.size(); i++){
@@ -54,6 +69,11 @@ public class Inventory {
         }
         return parts;
     }
+    /**
+     *  Looks up products given a productName and returns the result
+     * @param productName [string] value productName to be looked up
+     * @return ObservableList<Product> matching the given productName value or null if not found
+     */
     public ObservableList<Product> lookupProduct(String productName){
         ObservableList<Product> products = FXCollections.observableArrayList();
         for(int i = 0; i<this.allProducts.size();i++){
@@ -63,21 +83,51 @@ public class Inventory {
         }
         return products;
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public void updatePart(int index, Part newPart){
         this.allParts.set(index, newPart);
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public void updateProduct(int index, Product newProduct){
         this.allProducts.set(index, newProduct);
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public boolean deletePart(Part selectedPart){
         return this.allParts.remove(selectedPart);
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public boolean deleteProduct(Product selectedProduct){
         return this.allProducts.remove(selectedProduct);
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public ObservableList<Part> getAllParts(){
         return this.allParts;
     }
+    /**
+     *
+     * @param
+     * @return
+     */
     public ObservableList<Product> getAllProducts(){
         return this.allProducts;
     }
@@ -85,10 +135,6 @@ public class Inventory {
 }
 /**
  * TODO:DOCS
- + lookupPart(partId:int):Part
- + lookupProduct(productId:int):Product
- + lookupPart(partName:String):ObservableList<Part>
- + lookupProduct(productName:String):ObservableList<Product>
  + updatePart(index:int, selectedPart:Part):void
  + updateProduct(index:int, newProduct:Product):void
  + deletePart(selectedPart:Part):boolean
