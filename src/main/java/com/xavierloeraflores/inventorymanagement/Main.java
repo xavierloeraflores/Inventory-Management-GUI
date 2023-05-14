68cf27a58909e7a3c2920bbe89866a1d02315583
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-14 12:35:32 -0700

    FEAT:Main class now starts the application

--- src/main/java/com/xavierloeraflores/inventorymanagement/Main.java
+++ src/main/java/com/xavierloeraflores/inventorymanagement/Main.java
@@ -3,9 +3,21 @@ package com.xavierloeraflores.inventorymanagement;
  *
  * @author xavierloeraflores
  */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+public class Main extends Application{
+    public void start(Stage stage) throws Exception{
+        Parent mainFXML = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene mainScene = new Scene(mainFXML);
+        stage.setScene(mainScene);
+        stage.show();
+    }
     public static void main(String[] args){
         System.out.println("Inventory Management Application");
+        launch(args);
     }
 }
 
